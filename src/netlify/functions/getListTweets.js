5791cc32eb853c0e534e526819d88ac708c4cce6
alvdef <alvdef@users.noreply.github.com>
2022-10-02 19:20:43 +0200

    request to nettlify test

--- src/netlify/functions/getListTweets.js
+++ src/netlify/functions/getListTweets.js
@@ -1,67 +1,39 @@
 const axios = require("axios");
 
-exports.handler = async function (event, context) {
-    console.log(event);
-    console.lot(context);    
+// exports.handler = async function (event, context) {
+//     console.log(event);
+//     console.lot(context);    
     
-    const token = process.env.bearer_token;
-    const { listId } = event.queryStringParameters;
-    const endpointURL = `https://api.twitter.com/2/lists/${listId}/tweets`;
+//     const token = process.env.bearer_token;
+//     const { listId } = event.queryStringParameters;
+//     const endpointURL = `https://api.twitter.com/2/lists/${listId}/tweets`;
 
-    try {
-        const res = await axios.get(endpointURL, {
-            headers: {
-                authorization: `Bearer ${token}`,
-            },
-            params: {
-                "tweet.fields": "created_at",   
-                expansions: "author_id",            
-                "user.fields": "name,url,username",    
-            }
-        });
-        return {
-            statusCode: 200,
-            body: res.body,
-        }
-    } catch (err) {
-        return {
-            statusCode: 400,
-            body: {
-                "data": [
-                    {
-                        "created_at": "2022-09-27T14:20:00.000Z",
-                        "author_id": "443680219",
-                        "text": "Te dicen que te van a hacer uno de estos dos peinados.\n\n¿Cuál eliges?",
-                        "id": "1574765805400735746"
-                    },
-                    {
-                        "created_at": "2022-09-27T14:17:36.000Z",
-                        "author_id": "376256489",
-                        "text": "RT @mincoturgob: 🏖 Hoy se celebra el #DiaMundialDelTurismo bajo el lema #RepensarElTurismo\n\nEn España somos líderes mundiales por nuestra o…",
-                        "id": "1574765201181786113"
-                    }
-                ],
-                "includes": {
-                    "users": [
-                        {
-                            "url": "https://t.co/5VuFimg0q5",
-                            "id": "443680219",
-                            "name": "NBA Spain",
-                            "username": "NBAspain"
-                        },
-                        {
-                            "url": "https://t.co/GamM83oZcy",
-                            "id": "376256489",
-                            "name": "CSD",
-                            "username": "deportegob"
-                        }
-                    ]
-                },
-                "meta": {
-                    "result_count": 2,
-                    "next_token": "7140dibdnow9c7btw423hvanaxz0p73t2oe62qwn65vlf"
-                }
-            },
-        }
+//     try {
+//         const res = await axios.get(endpointURL, {
+//             headers: {
+//                 authorization: `Bearer ${token}`,
+//             },
+//             params: {
+//                 "tweet.fields": "created_at",   
+//                 expansions: "author_id",            
+//                 "user.fields": "name,url,username",    
+//             }
+//         });
+//         return {
+//             statusCode: 200,
+//             body: res.body,
+//         }
+//     } catch (err) {
+//         return {
+//             statusCode: 400,
+//             body: err,
+//         }
+//     }
+// }
+
+exports.handler = async function (event, context) {
+    return {
+        statusCode: 200,
+        body: "hello wrld getlisttweets"
     }
 }
--- src/netlify/functions/listsByUserId.js
+++ src/netlify/functions/listsByUserId.js
@@ -1,41 +1,50 @@
 const axios = require("axios");
 
-exports.handler = async function (event, context) {
-    console.log(event);
-    console.lot(context);    
+// exports.handler = async function (event, context) {
+//     console.log(event);
+//     console.lot(context);    
 
-    const userId = '1566757431413293057';
-    const token = process.env.bearer_token;
-    const endpointURL = `https://api.twitter.com/2/users/${userId}/owned_lists`;
+//     const userId = '1566757431413293057';
+//     const token = process.env.bearer_token;
+//     const endpointURL = `https://api.twitter.com/2/users/${userId}/owned_lists`;
  
-    try {
-        const res = await axios.get(endpointURL, {
-            headers: {
-                authorization: `Bearer ${token}`,
-            }
-        });
-        return {
-            statusCode: 200,
-            body: res.body,
-        }
-    } catch (err) {
-        return {
-            statusCode: 400,
-            body: {
-                "data": [
-                    {
-                        "id": "1566842355059154945",
-                        "name": "Tecnologia"
-                    },
-                    {
-                        "id": "1566842155594833922",
-                        "name": "Deportes"
-                    }
-                ],
-                "meta": {
-                    "result_count": 2
-                }
-            }
-        }
+//     try {
+//         const res = await axios.get(endpointURL, {
+//             headers: {
+//                 authorization: `Bearer ${token}`,
+//             }
+//         });
+//         return {
+//             statusCode: 200,
+//             body: res.body,
+//         }
+//     } catch (err) {
+//         return {
+//             statusCode: 400,
+//             body: {
+//                 "data": [
+//                     {
+//                         "id": "1566842355059154945",
+//                         "name": "Tecnologia"
+//                     },
+//                     {
+//                         "id": "1566842155594833922",
+//                         "name": "Deportes"
+//                     }
+//                 ],
+//                 "meta": {
+//                     "result_count": 2
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+exports.handler = async function (event, context) {
+    return {
+        statusCode: 200,
+        body: "hello world listsbyuserid"
     }
 }
+
